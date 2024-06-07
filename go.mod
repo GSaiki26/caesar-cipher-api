133ed302d859674154635b57341b2e38aae991b8
Saiki <GSaiki26@gmail.com>
2024-06-07 15:14:52 -0300

    Added: first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/GSaiki26/go-caesar-cipher
+
+go 1.22.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Functions
+
+func main() {
+	fmt.Print(ConvertToCesar("Hello world!", 24))
+}
